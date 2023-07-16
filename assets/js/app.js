f8dd988f8bf43ebbc2e74d6e588e7f723dc05eaf
Harsh Vaishnav <harshvaishnav2396@gmail.com>
2023-07-16 21:34:52 +0530

    worked on about page and modified index page

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const cl = console.log;
+
+
+const tmSlider = document.getElementById('tmSlider')
+
+
+$('#tmSlider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
